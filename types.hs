a1427c93e739f8d2b4860747f73bd93178483781
Jino Tesauro <jinotesauro@pop-os.localdomain>
2023-11-01 13:26:06 -0500

    Changed winner

--- types.hs
+++ types.hs
@@ -1,22 +1,27 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use newtype instead of data" #-}
+{-# HLINT ignore "Eta reduce" #-}
 
 --Disk = what you play with 
 --Color = what gets dropped in /Player 
 --tile = indiviual holes in the board
 --board 6x7 columns and rows, 42 holes in totßal  
 
--- DUE WED : DATA TYPES !!!
 
 --Column number (inputed by player when making move)
 --Only storing colors in holes; not storing empty holes
 -- 7 Columns by default
 -- Head of column is highest color in stack
 --     - when we drop a new color, it gets appended to the front of the list
+--Don't need to index bc we can use  splitat
 type Column = [Color]
 
 --Possibilities of states for the holes on the board
 data Color = Yellow | Red deriving (Eq, Show)
+
+data Condition = Win | Stalemate
+
+type Win = Color
 
 --List of the states for the holes for the whole board
 type Board = [Column]
@@ -24,9 +29,11 @@ type Board = [Column]
 --Board in game and the color of the players turn
 type Game = (Board, Color)
 
--- Who won?
-type Winner = Color
+-- Who won? red , yellow
+type Winner = Condition
 
+
+-- METH  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 makeMove :: Board -> Color -> Column -> Board
 makeMove = undefined
@@ -37,5 +44,21 @@ gameWin = undefined
 
 --checks if the column is full
 columnFull :: Column -> Bool 
-columnFUll givenColumn = if length $ givenColumn == 6 then True else False
+columnFull givenColumn = length givenColumn == 6
 --6 can change, if you want it to be 7x7, then change that value to 7
+
+--checks if the entire board is full, indicating a draw
+--maybe add condition using gameWin?
+boardFull :: Board -> Bool
+boardFull board = all columnFull board
+
+    -- STORIES
+    -- 1. Define data types or type aliases for a player, game state, move, and winner.
+
+    -- 2. Be able to determine who has won the game state, if anyone.
+
+    -- 3. Be able to compute the result of making a legal move in a game state.
+
+    -- 4. Be able to compute the legal moves from a game state.
+
+    -- 5. (If time) Be able to pretty-print a game into a string.
