b47112d3135ee1524ae73183b773658628caf334
Kaelubagu <caguiar@trinity.edu>
2023-11-01 12:53:08 -0500

    added

--- types.hs
+++ types.hs
@@ -28,5 +28,15 @@ type Game = (Board, Color)
 type Winner = Color
 
 
---I JUST PUSHED
+makeMove :: Board -> Color -> Column -> Board
+makeMove = undefined
+
+--sees if anyone in the game has won.
+gameWin :: Board -> Winner 
+gameWin = undefined
+
+--checks if the column is full
+columnFull :: Column -> Bool 
+columnFUll givenColumn = if length $ givenColumn == 6 then True else False
+--6 can change, if you want it to be 7x7, then change that value to 7
 
