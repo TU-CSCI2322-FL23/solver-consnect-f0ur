69b8831495ea9472619b37f4bc84d6b4a9d67cad
Ntiglao <116304714+Ntiglao@users.noreply.github.com>
2023-11-05 14:27:38 -0600

    changes

---
+++ game.hs
@@ -0,0 +1,16 @@
+import types
+{-this where our game will be created. -}
+
+
+makeMove :: Board -> Color -> Column -> Board
+makeMove = undefined
+
+--sees if anyone in the game has won.
+gameWin :: Board -> Winner 
+gameWin = undefined
+
+--checks if the column is full
+columnFull :: Column -> Bool 
+columnFull givenColumn = if length $ givenColumn == 6 then True else False
+--6 can change, if you want it to be 7x7, then change that value to 7
+
