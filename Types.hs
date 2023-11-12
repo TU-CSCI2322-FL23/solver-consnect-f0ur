807b9da0a40abe52b31a0a0d4fea6ee6d2e30aec
Jino Tesauro <jinotesauro@pop-os.localdomain>
2023-11-12 15:03:40 -0600

    Fixed empty file Types.hs

---
+++ Types.hs
@@ -0,0 +1,30 @@
+module Types where
+
+--Disk = what you play with 
+--Color = what gets dropped in /Player 
+--tile = indiviual holes in the board
+--board 7 columns 6 rows, 42 holes in totßal  
+
+
+--Column number (inputed by player when making move)
+--Only storing colors in holes; not storing empty holes
+-- 7 Columns by default
+-- Head of column is highest color in stack
+--     - when we drop a new color, it gets appended to the front of the list
+--Don't need to index bc we can use  splitat
+type Column = [Color]
+
+--Possibilities of states for the holes on the board
+-- We don't derive Show because we have a custom one!
+data Color = Yellow | Red deriving (Eq)
+
+data Winner = Win Color | Stalemate deriving (Eq) -- define win
+
+--List of the states for the holes for the whole board
+type Board = [Column]
+
+--Board in game and the color of the players turn
+type Game = (Board, Color)
+
+-- 0 to 6 (makes it easier to code; we can change it to 1 to 7 later)
+type Move = Int
